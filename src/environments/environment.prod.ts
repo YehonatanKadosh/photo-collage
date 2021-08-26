112b444a172cf24ea29c9d6844266c6c4038b057
YehonatanKadosh <jacojacoj3@gmail.com>
2021-08-26 17:34:32 +0300

    update

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: 'true',
+  PIXABAY_API_KEY: '22587962-22f41904ba2284a9141fdbac6',
+  PIXABAY_API_URL: 'https://pixabay.com/api/?key=',
+  GOOGLE_MAPS_API_KEY: 'AIzaSyASgW_mk-RWPbAipJEGJGl8mAIaTgVBWgM',
+  Photos_API_URl: 'https://photo-collage-server.azurewebsites.net/Photos',
+  User_API_URL: 'https://photo-collage-server.azurewebsites.net/User',
+  DARK_MODE: 'dark-mode',
+  LIGHT_MODE: 'light-mode',
+  Template_GRID: 'Grid',
+  Template_LIST: 'List',
+};
