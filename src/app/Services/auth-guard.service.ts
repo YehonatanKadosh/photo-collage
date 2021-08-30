33ed4e4fa1291a2551caaa55aff00df426bcb8fb
YehonatanKadosh <jacojacoj3@gmail.com>
2021-08-30 03:07:29 +0300

    authentication guard

--- src/app/Services/auth-guard.service.ts
+++ src/app/Services/auth-guard.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
 import { Router } from '@angular/router';
-import { SiteStateService } from './site-state.service';
+import { UserService } from './user-service.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuardService implements CanActivate {
-  constructor(private router: Router, private siteState: SiteStateService) {}
+  constructor(private router: Router, private userService: UserService) {}
 
   canActivate() {
-    // if (!this.userService.user) {
-    //   this.router.navigateByUrl('/404');
-    //   return false;
-    // }
+    if (!this.userService.user) {
+      this.router.navigateByUrl('/404');
+      return false;
+    }
     return true;
   }
 }
